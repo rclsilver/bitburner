618b4504139df4c6384bf30c018e1fe610aab51b
Thomas Bétrancourt <thomas@betrancourt.net>
2022-04-20 22:55:42 +0200

    improve hack-all-servers: use programs if available

--- src/hack-all-servers.ts
+++ src/hack-all-servers.ts
@@ -2,7 +2,7 @@ import { NS } from "@ns";
 
 function getAllServers(
   ns: NS,
-  fromServer = 'home',
+  fromServer = "home",
   result = new Set<string>()
 ): string[] {
   const servers = ns.scan(fromServer);
@@ -17,7 +17,12 @@ function getAllServers(
   return Array.from(result);
 }
 
-function runScript(ns: NS, server: string, script: string, ...args: string[]): number {
+function runScript(
+  ns: NS,
+  server: string,
+  script: string,
+  ...args: string[]
+): number {
   const requiredMemory = ns.getScriptRam(script, server);
   const usableRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
   const threads = Math.floor(usableRam / requiredMemory);
@@ -40,23 +45,63 @@ export async function main(ns: NS): Promise<void> {
     if (!ns.hasRootAccess(server)) {
       ns.tprint(`No root access on ${server}`);
 
-      if(ns.fileExists("NUKE.exe", "home")) {
+      if (ns.fileExists("BruteSSH.exe", "home")) {
+        ns.tprint(`Opening SSH port of ${server}...`);
+        try {
+          const r = ns.brutessh(server);
+          ns.tprint(`SSH port of ${server} opened? ${r}`);
+        } catch (e) {
+          ns.tprint(`Failed to open SSH port of ${server}: ${e}`);
+        }
+      }
+
+      if (ns.fileExists("FTPCrack.exe", "home")) {
+        ns.tprint(`Opening FTP port of ${server}...`);
+        try {
+          const r = ns.ftpcrack(server);
+          ns.tprint(`FTP port of ${server} opened? ${r}`);
+        } catch (e) {
+          ns.tprint(`Failed to open FTP port of ${server}: ${e}`);
+        }
+      }
+
+      if (ns.fileExists("HTTPWorm.exe", "home")) {
+        ns.tprint(`Opening HTTP port of ${server}...`);
+        try {
+          const r = ns.httpworm(server);
+          ns.tprint(`HTTP port of ${server} opened? ${r}`);
+        } catch (e) {
+          ns.tprint(`Failed to open HTTP port of ${server}: ${e}`);
+        }
+      }
+
+      if (ns.fileExists("relaySMTP.exe", "home")) {
+        ns.tprint(`Opening SMTP port of ${server}...`);
+        try {
+          const r = ns.relaysmtp(server);
+          ns.tprint(`SMTP port of ${server} opened? ${r}`);
+        } catch (e) {
+          ns.tprint(`Failed to open SMTP port of ${server}: ${e}`);
+        }
+      }
+
+      if (ns.fileExists("SQLInject.exe", "home")) {
+        ns.tprint(`Opening SQL port of ${server}...`);
+        try {
+          const r = ns.sqlinject(server);
+          ns.tprint(`SQL port of ${server} opened? ${r}`);
+        } catch (e) {
+          ns.tprint(`Failed to open SQL port of ${server}: ${e}`);
+        }
+      }
+
+      if (ns.fileExists("NUKE.exe", "home")) {
         ns.tprint(`Nuking ${server}...`);
         try {
           ns.nuke(server);
           ns.tprint(`Nuked ${server}: ${ns.hasRootAccess(server)}`);
         } catch (e) {
           ns.tprint(`Failed to nuke ${server}: ${e}`);
-
-          if (ns.fileExists("BruteSSH.exe", "home")) {
-            ns.tprint(`Bruteforcing ${server}...`);
-            try {
-              ns.brutessh(server);
-              ns.tprint(`Bruteforced ${server}: ${ns.hasRootAccess(server)}`);
-            } catch (e) {
-              ns.tprint(`Failed to bruteforce ${server}: ${e}`);
-            }
-          }
         }
       }
     }
@@ -66,21 +111,25 @@ export async function main(ns: NS): Promise<void> {
     }
   }
 
-  ns.tprint(`Hackable servers: ${servers.join(', ')}`);
+  ns.tprint(`Hackable servers: ${servers.join(", ")}`);
 
   for (const server of servers) {
     ns.tprint(`Copying scripts to ${server}...`);
     await ns.scp(scripts, "home", server);
   }
 
-  while (true){
+  while (true) {
     for (const server of servers) {
+      if (ns.getServerRequiredHackingLevel(server) > ns.getHackingLevel()) {
+        continue;
+      }
+
       const moneyThreshold = ns.getServerMaxMoney(server) * 0.75;
       const securityThreshold = ns.getServerMinSecurityLevel(server) + 5;
 
       if (ns.getServerSecurityLevel(server) > securityThreshold) {
         runScript(ns, server, "/bin/weaken.js", server);
-      } else if(ns.getServerMoneyAvailable(server) < moneyThreshold) {
+      } else if (ns.getServerMoneyAvailable(server) < moneyThreshold) {
         runScript(ns, server, "/bin/grow.js", server);
       } else {
         runScript(ns, server, "/bin/hack.js", server);
